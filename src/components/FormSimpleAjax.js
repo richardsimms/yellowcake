806b44bccc78ec1151ea2f28e14503024a02f368
Richard Simms <rich@R-I-C-H--K-A-T-E.local>
2019-11-03 21:33:39 +1100

    form change

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -66,8 +66,8 @@ class Form extends React.Component {
           name={name}
           action={action}
           onSubmit={this.handleSubmit}
-          data-netlify=""
-          netlify-recaptcha=""
+          data-netlify="true"
+          netlify-recaptcha="true"
         >
           {this.state.alert && (
             <div className="Form--Alert">{this.state.alert}</div>
