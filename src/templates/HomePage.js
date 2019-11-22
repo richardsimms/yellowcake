0a477faba85709e4093e735f3ce05ed56b8519d8
Richard Simms <rich@R-I-C-H--K-A-T-E.local>
2019-11-22 19:35:31 +1100

    home page products

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -2,15 +2,15 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import PageHeader from '../components/PageHeader'
+import Products from '../components/Products'
 import Content from '../components/Content'
 import Layout from '../components/Layout'
 
 
 
 
-
 // Export Template for use in CMS preview
-export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
+export const HomePageTemplate = ({ title, subtitle, featuredImage, body, }) => (
   <main className="Home">
     <PageHeader
       large
@@ -22,6 +22,35 @@ export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
     <section className="section">
       <div className="container skinny">
         <Content source={body} />
+      </div>
+    </section>
+    <section className="section">
+      <div className="container skinny">
+        <Content source={body} /> 
+        <Products 
+          large
+          prodtitle="Strategic marketing planning session"
+          proddisc="Feel like you need to get your business on track for growth and just don’t know where to start?"
+          productbutton="Get your planning session"
+          productlink="#"
+          backgroundImage={featuredImage}
+        />
+        <Products 
+          large
+          prodtitle="Strategic marketing planning session"
+          proddisc="Feel like you need to get your business on track for growth and just don’t know where to start?"
+          productbutton="Get your planning session"
+          productlink="#"
+          backgroundImage={featuredImage}
+        />
+        <Products 
+          large
+          prodtitle="Strategic marketing planning session"
+          proddisc="Feel like you need to get your business on track for growth and just don’t know where to start?"
+          productbutton="Get your planning session"
+          productlink="#"
+          backgroundImage={featuredImage}
+        />
       </div>
     </section>
   </main>
